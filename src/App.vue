32d56793c664e24a06411f9f0145feb0068a5aba
kmv-dev <kmvdev@yandex.ru>
2021-04-03 20:58:42 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div id="nav">
+    <router-link class="r-link" to="/">Home</router-link> |
+    <router-link class="r-link" to="/about">About</router-link>
+  </div>
+  <router-view/>
+</template>
+
+<style lang="scss" src="./scss/main.scss">
+
+</style>
